1294909feb4ba5713802e1bb5c49497def7b7723
David Padilla <david.padilla@crowdint.com>
2010-07-27 22:48:03 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3JqueryAutocompleteApp::Application.config.secret_token = '89c05dbc4988ea5f3c7e5b0880d2bbfc9b1baf2f98af42fa08e1a86c1ef98e5c2db0f0dc82f2d6faba25af043a08b11a9b404a846dc929e668aeee3269483cca'
